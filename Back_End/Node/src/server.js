f9fde316913e0b4f7f0a25ec154ef5c548a33372
NguyenAndrew <anguyen29@wisc.edu>
2019-01-21 22:38:41 -0500

    Use environment variable for port, and default to 3001

--- Back_End/Node/src/server.js
+++ Back_End/Node/src/server.js
@@ -1,5 +1,6 @@
 const app = require('./app.js');
+const port = process.env.port || 3001;
 
-app.listen(3001, () => {
-  console.log('listening on port 3001');
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
